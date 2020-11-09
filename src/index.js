7c570199754a4d9e16bf31be5697adb0b9f7213e
PBudgie <peiyi.lin2473@gmail.com>
2020-11-08 19:44:40 -0800

    Simplify onChange logic

--- src/index.js
+++ src/index.js
@@ -53,88 +53,64 @@ class Board extends React.Component {
 
   handleChange (i, j, event) {
     const {squares, isValid} = this.state;
-    const newState = new Array(9).fill(0).map(() => new Array(9).fill(0))
+    const newSquares = squares.map((el) => el.map((el2) => el2)) //deep copy squares
     const newIsValid = isValid.map((el) => el.map((el2) => el2)); //deep copy isValid
     const myUpdatedValue = parseInt(event.target.value);
-    var foundDuplicate = false;
 
-    //take care of newState
-    squares.forEach((elem1, ind1) => elem1.forEach((elem2, ind2) => {
-      if(ind1 === i && ind2 === j) {
-        if(myUpdatedValue.length === 0) {
-          newState[ind1][ind2] = 0;
-        } else {
-          newState[ind1][ind2] = myUpdatedValue;
-        }
-      } else {
-        newState[ind1][ind2] = elem2;
-      }
-    }))
-
-    //On a change, we only need to check whether it causes its row, col, and grid to be invalid
-    for(let row = 0; row < 8; row++) { //check row
-      if(row !== i && squares[row][j] === myUpdatedValue) { //found duplicate
-        newIsValid[i][j] = false;
-        newIsValid[row][j] = false;
-        foundDuplicate = true;
-      }
+    //take care of the new entry in newSquares
+    if(!isNaN(myUpdatedValue) && myUpdatedValue <= 9 && myUpdatedValue >= 1) {
+      newSquares[i][j] = myUpdatedValue
+    } else {
+      newSquares[i][j] = 0
     }
 
-    for(let col = 0; col < 8; col++) { //check column
-      if(col !== j && squares[i][col] === myUpdatedValue) {
-        newIsValid[i][j] = false;
-        newIsValid[i][col] = false;
-        foundDuplicate = true;
-      }
-    }
-
-    //check grid
-    var startX;
-    var startY;
-
-    if (i >= 0 && i <= 2) {
-      startX = 0;
-    } else if (i >= 3 && i <= 5) {
-      startX = 3;
-    } else if (i >= 6 && i <= 8) {
-      startX = 6;
-    }
-
-    if (j >= 0 && j <= 2) {
-      startY = 0;
-    } else if (j >= 3 && j<= 5) {
-      startY = 3;
-    } else if (j >= 6 && j <= 8) {
-      startY = 6;
-    }
-
-    for(let gridX = startX; gridX <= startX + 2; gridX++) {
-      for(let gridY = startY; gridY <= startY + 2; gridY++) {
-        if(i !== gridX && j !== gridY && squares[gridX][gridY] === myUpdatedValue) {
-          newIsValid[i][j] = false;
-          newIsValid[gridX][gridY] = false;
-          foundDuplicate = true;
+    // See whether this change has a duplicate
+    if(!isNaN(myUpdatedValue)) {
+      for(let row = 0; row < 8; row++) { //check entries in row
+        if(row !== i && squares[row][j] === myUpdatedValue) { //found duplicate
+          newIsValid[i][j] = false
         }
       }
-    }
 
-    //If there are no duplicates, turn the row/col/grid validity to true
-    if(squares[i][j] === false && foundDuplicate === false) {
-      for(let row = 0; row < 8; row++) {
-        newIsValid[row][j] = true;
-      }
-      for(let col = 0; col < 8; col++) {
-        newIsValid[i][col] = true;
-      }
-      for(let gridX = startX; startX <= startX + 2; startX++) {
-        for(let gridY = startY; startY <= startY + 2; startY++) {
-          newIsValid[gridX][gridY] = true;
+      for(let col = 0; col < 8; col++) { //check column
+        if(col !== j && squares[i][col] === myUpdatedValue) {
+          newIsValid[i][j] = false
         }
       }
+
+      //check grid
+      var startX;
+      var startY;
+
+      if (i >= 0 && i <= 2) {
+        startX = 0;
+      } else if (i >= 3 && i <= 5) {
+        startX = 3;
+      } else if (i >= 6 && i <= 8) {
+        startX = 6;
+      }
+
+      if (j >= 0 && j <= 2) {
+        startY = 0;
+      } else if (j >= 3 && j<= 5) {
+        startY = 3;
+      } else if (j >= 6 && j <= 8) {
+        startY = 6;
+      }
+
+      for(let gridX = startX; gridX <= startX + 2; gridX++) {
+        for(let gridY = startY; gridY <= startY + 2; gridY++) {
+          if(i !== gridX && j !== gridY && squares[gridX][gridY] === myUpdatedValue) {
+            newIsValid[i][j] = false
+          }
+        }
+      }
+    } else { //On change to no number
+      newIsValid[i][j] = true
     }
 
     this.setState({
-      squares: newState,
+      squares: newSquares,
       isValid: newIsValid
     });
   }
@@ -148,6 +124,7 @@ class Square extends React.Component {
           className="square"
           onChange={this.props.onChange}
           value={this.props.value === 0 ? "" : this.props.value}
+          readOnly={this.props.locked}
           />
       </td>
     );
