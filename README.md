7bb1ea6539548db90dc008aed09745edc992b36c
PBudgie <peiyi.lin2473@gmail.com>
2021-01-25 18:03:42 -0800

    Fetch puzzles from backend

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Sudoku in React
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+This repo contains the front-end of a sudoku app. The app retrieves a puzzle from my backend API and shows "Congratulations" when the puzzle is successfully completed. It sends the completion time back to the backend.
--- src/index.js
+++ src/index.js
@@ -3,16 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Board from './components/Board.js'
 
-ReactDOM.render(
-  <Board squares={   
-    [[0,3,5,4,1,6,9,2,7],
-    [2,9,6,8,5,7,4,3,1],
-    [4,1,7,2,9,3,6,5,8],
-    [5,6,9,1,3,4,7,8,2],
-    [1,2,3,6,7,8,5,4,9],
-    [7,4,8,5,2,9,1,6,3],
-    [6,5,2,7,8,1,3,9,4],
-    [9,8,1,3,4,5,2,7,6],
-    [3,7,4,9,6,2,8,1,5]] }/>,
-    document.getElementById('root')
-);
+const apiEndpoint = "http://localhost:3000"
+fetch(apiEndpoint)
+    .then(response => response.json())
+    .then((json) => {
+        /* Easy puzzle to test completion
+        ReactDOM.render(
+            <Board puzzleId={json.puzzleId} squares={   
+              [[0,3,5,4,1,6,9,2,7],
+              [2,9,6,8,5,7,4,3,1],
+              [4,1,7,2,9,3,6,5,8],
+              [5,6,9,1,3,4,7,8,2],
+              [1,2,3,6,7,8,5,4,9],
+              [7,4,8,5,2,9,1,6,3],
+              [6,5,2,7,8,1,3,9,4],
+              [9,8,1,3,4,5,2,7,6],
+              [3,7,4,9,6,2,8,1,5]] }/>,
+              document.getElementById('root')
+          ); */
+
+        ReactDOM.render(
+            <Board puzzleId={json.puzzleId} squares={convertTo2DArray(json.puzzle)}/>,
+              document.getElementById('root')
+        );
+    })
+    .catch(() => {
+        console.log("Error occurred getting the puzzle from the api")
+    })
+
+function convertTo2DArray(puzzleString) {
+    var puzzle2DArray = new Array(9).fill(0).map(() => Array(9).fill(0))
+    for(var i = 0; i < puzzleString.length; i++) {
+        var row = Math.floor(i/9)
+        var col = i - (9*row)
+        puzzle2DArray[row][col] = parseInt(puzzleString.charAt(i))
+    }
+    return puzzle2DArray
+}
